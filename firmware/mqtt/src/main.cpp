5cf6676c6e2904c09f251a004f7a4124d80d3cda
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-10-09 15:57:44 +0200

    Starting code to send mqtt message

--- firmware/mqtt/src/main.cpp
+++ firmware/mqtt/src/main.cpp
@@ -1,18 +1,91 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <SRAM.h>
+#include "secret.h"
 
-// put function declarations here:
-int myFunction(int, int);
+SRAM sram(4, SRAM_1024);
+
+unsigned long lastMillis = 0;
+unsigned long timeAlive = 0;
+unsigned long cycle = 0;
+const unsigned long interval = 1000; // Send the message every 1 second
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  sram.seek(1);
+
+  // do something with the message
+  for (uint8_t i = 0; i < length; i++) {
+    Serial.write(sram.read());
+  }
+  Serial.println();
+
+  // Reset position for the next message to be stored
+  sram.seek(1);
+}
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  sram.begin();
+  sram.seek(1);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Set MQTT server
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  // Connect to MQTT broker
+  while (!client.connected()) {
+    if (client.connect("esp32Client")) {
+      Serial.println("Connected to MQTT broker");
+      client.publish(outTopic, "hello world");
+      client.subscribe(inTopic);
+    } else {
+      Serial.print("Failed to connect to MQTT broker, retrying in 5 seconds...");
+      delay(5000);
+    }
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  client.loop();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  unsigned long currentMillis = millis();
+
+  // increment cycle counter
+  cycle++;
+  
+  // Check if it's time to send the message
+  if (currentMillis - lastMillis >= interval) {
+    // Save the current time
+    lastMillis = currentMillis;
+
+    // Increment the alive counter
+    timeAlive++;
+
+    // Format the message (json)
+    String outMessage = "{\"timeAlive\": " + String(timeAlive) + "}";
+    char outMessageBuffer[50];
+    outMessage.toCharArray(outMessageBuffer, 50);
+
+    // Publish the "outMessage" 
+    if (client.publish(outTopic, outMessageBuffer)) {
+      Serial.println("Message sent");
+    } else {
+      Serial.println("Failed to send message");
+    }
+  }
+
+  // publish current cycle
+  client.publish("musiclighttiles/cycle", String(cycle).c_str());
 }
---
+++ firmware/mqtt/src/secret copy.h
@@ -0,0 +1,13 @@
+// Define your secrets here:
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* ssid = "ssid";
+const char* password = "wifiPassword";
+const char* mqtt_server = "mqtt.example.com";
+const int mqtt_port = 1883;
+const char* rootTopic = "rootTopic";
+const char* outTopic = "outTopic";
+const char* inTopic = "inTopic";
+
+#endif
