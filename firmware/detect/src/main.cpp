1f3c7b2f2d4c74602f927c6ac073fb54b26c1468
RubenBelligh <rubenbelligh@outlook.be>
2023-10-09 13:38:21 +0200

    ProgramDetection

--- firmware/detect/src/main.cpp
+++ firmware/detect/src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+  const int buttonPin = 16;
+  int buttonState=0;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+   pinMode(buttonPin,INPUT);
+   Serial.begin(9600);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  buttonState= digitalRead(buttonPin);
+
+  if(buttonState == 1){
+      //Turn led on
+
+       //digitalWrite(ledpin,HIGH);
+       digitalWrite(Serial.print("haha It works\n"),HIGH);
+       
+  }
+
+
+  else if (buttonState == 0){
+      // Turn LED off
+
+      //digitalWrite(led,LOW);
+      digitalWrite(Serial.print("RIP the leds\n"),LOW);
+      
+  }
+
+    delay(2000);
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
+
+
+
+
