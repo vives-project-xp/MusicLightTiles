8b19c8af446dab1332862f2661108093cf95e277
RubenBelligh <rubenbelligh@outlook.be>
2023-10-09 14:54:43 +0200

    programDetection

--- firmware/detect/src/main.cpp
+++ firmware/detect/src/main.cpp
@@ -2,19 +2,24 @@
 
 
   const int buttonPin = 16;
-  int buttonState=0;
+  int buttonState = 0;
+  const int switchPin = 17;
+  int switchState = 0;
 
 void setup() {
   // put your setup code here, to run once:
   
    pinMode(buttonPin,INPUT);
+   pinMode(switchPin,INPUT);
    Serial.begin(9600);
+   
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  buttonState= digitalRead(buttonPin);
+  buttonState = digitalRead(buttonPin);
+  switchState = digitalRead(switchPin);
 
   if(buttonState == 1){
       //Turn led on
@@ -31,6 +36,28 @@ void loop() {
       //digitalWrite(led,LOW);
       digitalWrite(Serial.print("RIP the leds\n"),LOW);
       
+  }
+
+
+  
+
+
+
+  if(switchState == 1){
+       // Turn on power
+       //digitalWrite(voeding,HIGH)
+       digitalWrite(Serial.print("Voeding aan\n"),HIGH);
+
+  }
+
+  else if(switchState == 0){
+
+    //Turn off power
+    //digitalWrite(voeding,LOW);
+    digitalWrite(Serial.print("Voeding uit"),LOW);
+
+
+
   }
 
     delay(2000);
