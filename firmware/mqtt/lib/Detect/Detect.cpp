5ad9f2f3c00405c37708cf2cc89929e7cedeb53d
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-10-15 22:37:15 +0200

    #14 Added detect class to library
    Added detect folder to library, this includes a !still to be implemented! detect class.

---
+++ firmware/mqtt/lib/Detect/Detect.cpp
@@ -0,0 +1,7 @@
+#include "Detect.h"
+
+Detect::Detect() {
+  // TODO: Implement constructor
+}
+
+// Rest of the class implementation
---
+++ firmware/mqtt/lib/Detect/Detect.h
@@ -0,0 +1,12 @@
+#ifndef Detect_h
+#define Detect_h
+// code goes here
+
+class Detect {
+  public:
+    Detect();
+
+  private:
+
+};
+#endif
