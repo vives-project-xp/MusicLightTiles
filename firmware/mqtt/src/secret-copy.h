ebe126ec796b44f8b069722c0dc58208a91830bc
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-10-16 12:41:45 +0200

    #14 Removed unused vars from secrets file
    Removed `rootTopic`, `outTopic` and `inTopic` from secrets file, as they are not used anymore.

--- firmware/mqtt/src/secret-copy.h
+++ firmware/mqtt/src/secret-copy.h
@@ -6,8 +6,5 @@ const char* ssid = "ssid";
 const char* password = "wifiPassword";
 const char* mqtt_server = "mqtt.example.com";
 const int mqtt_port = 1883;
-const char* rootTopic = "rootTopic";
-const char* outTopic = "outTopic";
-const char* inTopic = "inTopic";
 
 #endif
