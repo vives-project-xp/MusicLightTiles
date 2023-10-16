7ba62187aa6978bfd56235caf21bc7e5c507b733
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-10-16 13:08:26 +0200

    #14 Added basic implementation in light class
    Added Serialize, Deserialize and Update methodes to the light class. Further implementation is still needed as these functions only send to serial.

--- firmware/mqtt/lib/Light/Light.cpp
+++ firmware/mqtt/lib/Light/Light.cpp
@@ -1,7 +1,29 @@
 #include "Light.h"
 
 Light::Light() {
-  // TODO: Implement constructor
+  // Set default values
+  this->brightness = 0;
+  for (int i = 0; i < MAX_SECTIONS; i++) {
+    this->sections[i].r = 0;
+    this->sections[i].g = 0;
+    this->sections[i].b = 0;
+    this->sections[i].w = 0;
+  }
 }
 
-// Rest of the class implementation
+void Light::DeserializeInput(char* payload, unsigned int length) {
+  Serial.println("Deserializing input");
+  // TODO: Implement deserialization
+  this->UpdateState();
+}
+
+String Light::SerializeOutput() {
+  Serial.println("Serializing output");
+  // TODO: Implement serialization
+  return "{\"hello\":\"world\"}";
+}
+
+void Light::UpdateState() {
+  Serial.println("Updating state");
+  // TODO: Implement state update
+}
--- firmware/mqtt/lib/Light/Light.h
+++ firmware/mqtt/lib/Light/Light.h
@@ -2,11 +2,28 @@
 #define Light_h
 // code goes here
 
+// Import required libraries
+#include <Arduino.h>
+#include <arduinoJson.h>
+
+// Define constants
+#define MAX_SECTIONS 10
+
+struct Section {
+  int r;
+  int g;
+  int b;
+  int w;
+};
+
 class Light {
   public:
     Light();
-
+    void DeserializeInput(char* payload, unsigned int length);
+    String SerializeOutput();
+    void UpdateState();
   private:
-
+    int brightness;
+    Section sections[MAX_SECTIONS];
 };
 #endif
