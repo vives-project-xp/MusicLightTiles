318b99a9a514fafc25675ca63913b3c4b2dacbf2
Alberiek Depreytere <alberiek.depreytere@gmail.com>
2023-10-16 10:03:43 +0200

    audio flowchart and code

--- firmware/audio/src/main.cpp
+++ firmware/audio/src/main.cpp
@@ -1,11 +1,34 @@
 #include <Arduino.h>
-
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 // put function declarations here:
-int myFunction(int, int);
+//int myFunction(int, int);
+
+// Use pins 2 and 3 to communicate with DFPlayer Mini
+static const uint8_t PIN_MP3_TX = 26; // Connects to module's RX 
+static const uint8_t PIN_MP3_RX = 27; // Connects to module's TX 
+SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
+
+// Create the Player object
+DFRobotDFPlayerMini player;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Init USB serial port for debugging
+  Serial.begin(9600);
+  // Init serial port for DFPlayer Mini
+  softwareSerial.begin(9600);
+
+  // Start communication with DFPlayer Mini
+  if (player.begin(softwareSerial)) {
+   Serial.println("OK");
+
+    // Set volume to maximum (0 to 30).
+    player.volume(20);
+    // Play the first MP3 file on the SD card
+    player.play(1);
+  } else {
+    Serial.println("Connecting to DFPlayer Mini failed!");
+  }
 }
 
 void loop() {
