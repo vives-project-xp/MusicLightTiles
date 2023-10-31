8c03ee4a785b7a7f1074d0253a7c2aad8106119e
Rob Cocquyt <cocquyt.rob@gmail.com>
2023-10-31 18:50:22 +0100

    #36 Added loop example
    Created a loop example that lets a red light loop around. It moves 1 pixel every second. This happens for all online tiles.

--- command-control/main.py
+++ command-control/main.py
@@ -1,13 +1,15 @@
 import paho.mqtt.client as mqtt
 from tile import Tile
+from pixel import Pixel
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Constants
-HOST = os.getenv("MQTT_SERVER")
-PORT = os.getenv("MQTT_PORT")
+HOST: str = os.getenv("MQTT_SERVER")
+PORT: int = int(os.getenv("MQTT_PORT"))
 #MQTT_USER = os.getenv("MQTT_USERNAME")
 #MQTT_PASS = os.getenv("MQTT_PASSWORD")
 BASE_TOPIC = "music-light-tiles"
@@ -32,7 +34,7 @@ def on_message(client, userdata, msg):
   topic = msg.topic
   payload = msg.payload.decode("utf-8")
 
-  print("Recieved message on topic: " + topic)
+  #print("Recieved message on topic: " + topic)
  
   topic_parts = topic.split("/")
   tile: Tile = None
@@ -64,14 +66,86 @@ def on_message(client, userdata, msg):
     tile.update_state(payload)
     return
 
-client = mqtt.Client(client_id="controller", clean_session=True)
-client.on_connect = on_connect
-client.on_message = on_message
+# Main
+if __name__== "__main__":
+  # Setup MQTT client
+  client = mqtt.Client(client_id="controller", clean_session=True)
+  client.on_connect = on_connect
+  client.on_message = on_message
 
-client.connect(HOST, PORT, 60)
+  # Connect to MQTT server
+  client.connect(HOST, PORT, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+  # Keep the client connected (start a new thread)
+  client.loop_start()
+
+  # Wait until a tile is online
+  while len(tiles) == 0:
+    time.sleep(1)
+
+  tiles_online = False
+  while not tiles_online:
+    for tile in tiles:
+      if tile.online:
+        tiles_online = True
+        break
+    time.sleep(1)
+
+  print("Found online tiles")
+  
+  # Make list of online tiles
+  online_tiles = []
+  for tile in tiles:
+    if tile.online:
+      online_tiles.append(tile)
+
+  # Set ping of all tiles to off
+  for tile in online_tiles:
+    client.publish(BASE_TOPIC+"/"+tile.device_name+"/command", tile.create_command(system_ping=False))
+    while tile.pinging:
+      time.sleep(1)
+    
+  # Set all tiles to the same brightness
+  for tile in online_tiles:
+    client.publish(BASE_TOPIC+"/"+tile.device_name+"/command", tile.create_command(light_brightness=100))
+    while tile.brightness != 100:
+      time.sleep(0.1)
+
+  print("All tiles have the same brightness")
+  
+  # Set all tiles to the same color (black)
+  for tile in online_tiles:
+    pixels = [Pixel() for i in range(len(tile.pixels))]
+    client.publish(BASE_TOPIC+"/"+tile.device_name+"/command", tile.create_command(light_pixels=pixels))
+    while tile.pixels[0].red != 0 or tile.pixels[0].green != 0 or tile.pixels[0].blue != 0:
+      time.sleep(0.1)
+
+  print("All tiles have been set to black")
+
+  # Set first pixel of all tiles to red
+  for tile in online_tiles:
+    pixels = [Pixel() for i in range(len(tile.pixels))]
+    pixels[0].red = 255
+    client.publish(BASE_TOPIC+"/"+tile.device_name+"/command", tile.create_command(light_pixels=pixels))
+    while tile.pixels[0].red != 255:
+      time.sleep(0.1)
+
+  print("All first pixels have been set to red")
+  
+  # Shift pixels of all tiles by 1 loop forever
+  counter = 0
+  while True:
+    print("Counter: " + str(counter))
+    for tile in online_tiles:
+      pixels = tile.pixels[1:] + tile.pixels[:1]
+      client.publish(BASE_TOPIC+"/"+tile.device_name+"/command", tile.create_command(light_pixels=pixels))
+
+    if counter == len(online_tiles[0].pixels) - 1:
+      counter = 0
+    else:
+      counter += 1
+    time.sleep(1)
+    
+
+
+
