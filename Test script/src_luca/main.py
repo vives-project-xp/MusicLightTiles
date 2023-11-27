2c7ee843953f6864dcf92ff0113db56f62743f6e
Luca De Clerck <lucadeclerck@gmail.com>
2023-11-27 14:20:00 +0100

    Final code

--- Test script/src_luca/main.py
+++ Test script/src_luca/main.py
@@ -1,4 +1,3 @@
-'''
 import paho.mqtt.client as mqtt
 import json
 import time
@@ -9,6 +8,13 @@ mqtt_port = 1883
 mqtt_user = None
 mqtt_password = None
 topic_prefix = "PM/MLT"
+
+# List of valid tiles
+valid_tiles = ["TILE1", "TILE2", "TILE3"]  # Add your valid tiles here
+
+# Function to check if a tile exists
+def tile_exists(tile_name):
+    return tile_name in valid_tiles
 
 # Function to send a command to the Arduino
 def send_command(subtopic, command_type, command):
@@ -60,14 +66,29 @@ client.username_pw_set(mqtt_user, mqtt_password)
 client.on_message = on_message
 client.connect(mqtt_broker, mqtt_port, 60)
 
-# User input to choose the tile
-selected_tile = input("Enter the tile name (e.g., TILE1, TILE2): ")
+# User input to choose multiple tiles
+selected_tiles = input("Enter the tile names (comma-separated, e.g., TILE1,TILE2): ").split(',')
 
-# Subscribe to command topics for the selected tile
-client.subscribe(f"{topic_prefix}/{selected_tile}/self/command/audio")
-client.subscribe(f"{topic_prefix}/{selected_tile}/self/command/light")
-client.subscribe(f"{topic_prefix}/{selected_tile}/self/command/system")
-client.subscribe(f"{topic_prefix}/{selected_tile}/self/state/presence")
+# Check if all tiles exist
+invalid_tiles = [tile for tile in selected_tiles if not tile_exists(tile)]
+if invalid_tiles:
+    print(f"Error: Tiles {', '.join(invalid_tiles)} do not exist.")
+    exit()
+
+# Subscribe to command topics for the selected tiles
+subscribed_topics = [
+    f"{topic_prefix}/{tile}/self/command/audio" for tile in selected_tiles
+] + [
+    f"{topic_prefix}/{tile}/self/command/light" for tile in selected_tiles
+] + [
+    f"{topic_prefix}/{tile}/self/command/system" for tile in selected_tiles
+] + [
+    f"{topic_prefix}/{tile}/self/state/presence" for tile in selected_tiles
+]
+
+
+for topic in subscribed_topics:
+    client.subscribe(topic)
 
 # Example command for controlling the LED strip
 led_command = {
@@ -88,24 +109,19 @@ system_update_command = {
     "ping": True
 }
 
-# Send the LED command
-send_command(selected_tile, "light", led_command)
+# Send commands and publish presence detection state for each selected tile
+for tile in selected_tiles:
+    # Send the LED command
+    send_command(tile, "light", led_command)
 
-# Wait for a while
-time.sleep(5)
+    # Send the audio command
+    send_command(tile, "audio", audio_command)
 
-# Send the audio command
-send_command(selected_tile, "audio", audio_command)
+    # Send the system update command
+    send_command(tile, "system", system_update_command)
 
-time.sleep(5)
-
-# Send the system update command
-send_command(selected_tile, "system", system_update_command)
-
-# Publish presence detection state
-publish_presence_state(detected=True, subtopic=selected_tile)
+    # Publish presence detection state
+    publish_presence_state(detected=True, subtopic=tile)
 
 # Loop to listen for messages
 client.loop_forever()
-
-'''
